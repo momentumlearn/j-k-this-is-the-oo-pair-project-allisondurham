1b2223b6fbb5db5e5d402a9bf745667e293088d4
allisondurham <allison.durham@gmail.com>
2021-04-29 17:56:17 -0400

    liveshareupdate

--- liveshare.py
+++ liveshare.py
@@ -9,6 +9,11 @@ import random
 
 
 class Game:
+    def choose_difficulty():
+        difficulty_choice = [easy, medium, hard]
+        input()
+        return choice
+
     def pick_a_word():
         word_bank = ["tiger", "clover", "fork", "cloud"]
         print(len(random.choice(word_bank)))
@@ -21,7 +26,7 @@ class Player:
     def guess():
         while answer != True: #for max 8 guesses
             if guess == correct_letter:
-                print(#display blank spaces other than correct letter guessed)
+                print()#display blank spaces other than correct letter guessed)
             elif guess != correct_letter:
                 print("Wrong. Guess Again") #ask for input
             elif guess == repeat_guess:
@@ -30,3 +35,13 @@ class Player:
                 print("Too many letters")  
             else:
                 print("You Broke It!") 
+
+
+def win_or_lose:
+    reveal the answer
+    input("do you want to play again")
+    #if yes run new game
+
+    
+
+Game.pick_a_word()
