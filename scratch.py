2f715d5729e98aae1e52f818a290de5e335e24ae
allisondurham <allison.durham@gmail.com>
2021-04-30 14:40:35 -0400

    zzyogeton

---
+++ scratch.py
@@ -0,0 +1,16 @@
+
+import random
+
+def pick_mystery_word():
+    word_bank = ["tiger", "clover", "fork", "cloud"]
+    return random.choice(word_bank)
+    
+
+def split(word):
+    return list(word)
+
+
+mystery_word = pick_mystery_word()
+print(mystery_word)
+
+print(split(mystery_word))
---
+++ words.py
@@ -0,0 +1,22 @@
+string = """a
+aa
+aal
+aalii
+aam
+Aani
+aardvark
+aardwolf
+Aaron
+Aaronic
+Aaronical
+Aaronite
+Aaronitic
+Aaru
+Ab
+aba
+Ababdeh
+Ababua
+abac
+abaca"""
+
+print(str.split(string))
