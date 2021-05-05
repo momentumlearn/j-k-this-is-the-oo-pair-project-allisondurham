fbd2fa684f52073dd9ea1d1894ccc505d50d059c
allisondurham <allison.durham@gmail.com>
2021-05-04 20:56:58 -0400

    forreal this time

--- final.py
+++ final.py
@@ -1,14 +1,25 @@
 import random
 
-fileObject = open("words.txt", "r")
-lines = fileObject.readlines()
+massive_giant_word_list = open("words.txt", "r")
+lines = massive_giant_word_list.readlines()
 words = [line[:-1] for line in lines]
 
 short_words = []
 medium_words = []
 long_words = []
 
-
+for word in words:
+    if len(word) == 4 or len(word) == 5:
+        short_words.append(word)
+        # return short_words
+    elif len(word) == 6 or len(word) == 7:
+        medium_words.append(word)
+        # return medium_words
+    elif len(word) >= 8:
+        long_words.append(word)
+        # return long_words
+    else: 
+        pass
 ########################################################################
 
 
@@ -18,55 +29,36 @@ def intro():
             You will have a chance to guess a random word.\n
             Start by guessing a vowel and then a consonant.\n
             You will be given 8 chances to guess the word correctly.\n
-            The letter guessed correctly will display in the blank spaces for the word.\n
+            Any letter guessed correctly will display in the blank spaces for the word.\n
             Good luck and have fun!\n"""
     print(line)
 
 
 def difficulty_choice():
-    user_choice = input("Choose a difficutly:  ENTER  1 for Easy, 2 for Normal, or 3 for Challenging:  ")
-    return user_choice
-
-for word in words:
-    if len(word) == 4 or len(word) == 5:
-        short_words.append(word)
-        
-    elif len(word) == 6 or len(word) == 7:
-        medium_words.append(word)
-        
-    elif len(word) >= 8:
-        long_words.append(word)
-        
-    else: 
-        pass
-
-def game_mode_pick(difficulty_choice):
-    if difficulty_choice == "1":
+    choice = input("Choose a difficutly:  ENTER  1 for Easy, 2 for Normal, or 3 for Challenging:       ")
+    if choice == "1":
         return short_words
-    elif difficulty_choice == "2":
+    elif choice == "2":
         return medium_words
-    elif difficulty_choice == "3":
+    elif choice == "3":
         return long_words
-
     else: 
-        input("Choose a difficutly:  ENTER  1 for Easy, 2 for Normal, or 3 for Challenging:  ")
-
-# def length_of_word():
-#     word = [mystery_word]
-#     print(len(word))
+        print('please choose a valid value')
+        return difficulty_choice()
     
 
-def pick_mystery_word(difficulty):
-    word_bank = game_mode_pick(difficulty)
+def pick_mystery_word(word_bank):
     return random.choice(word_bank)
 
 
 ###########################################################################################
 
+
 def play(word):
     word_completion = "_ " * len(word)
     guessed = False
     guessed_letters = []
+    guessed_words = []
     tries = 8
     print("")
     print("Let's play WHEEL OF FORTUNE!")
@@ -96,11 +88,11 @@ def play(word):
                if "_" not in word_completion:
                    guessed = True
 
-        elif len(guess) == len(word) and guess.alpha():
+        elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed", guess)
             elif guess != word:
-                print(guess, "is not in the word.")
+                print(guess, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
@@ -125,12 +117,14 @@ def play(word):
         print()
         pass
 
+
 #########################################################################################################
+
 
 def main():
     intro()
-    choice = difficulty_choice()
-    mystery_word = pick_mystery_word(choice).upper()
+    word_bank = difficulty_choice()
+    mystery_word = pick_mystery_word(word_bank).upper()
     word = mystery_word
     print(mystery_word)
     play(word)
